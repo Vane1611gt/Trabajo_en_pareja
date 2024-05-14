eed1f11cff4783b0ad9fa06379abdbd2edc61ed1
Vane1611gt <159476021+Vane1611gt@users.noreply.github.com>
2024-05-14 07:27:00 -0600

    Add files via upload

---
+++ Trabajo_en_pareja-main/footer/cpu.js
@@ -0,0 +1,12 @@
+const cpu = document.createElement('cpu');
+footer.className = 'footer';
+
+
+const link = document.createElement('a');
+
+link.textContent = '  unidad central de procesamiento, es un componente de hardware y la unidad computacional central de un servidor';
+
+
+footer.appendChild(link);
+
+
---
+++ Trabajo_en_pareja-main/footer/footer.js
@@ -0,0 +1,13 @@
+
+const footer = document.createElement('footer');
+footer.className = 'footer';
+
+
+const link = document.createElement('a');
+
+link.textContent = '“Una vez un ordenador me venció jugando al ajedrez, pero no me opuso resistencia cuando pasamos al kick boxing” – Emo Philips ';
+
+
+footer.appendChild(link);
+
+document.body.appendChild(footer);
